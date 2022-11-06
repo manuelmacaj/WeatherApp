47e2e32537f4e0dfa6595d56dc6648a8049ea3d2
manuelmacaj <49785644+manuelmacaj@users.noreply.github.com>
2022-11-06 21:14:56 +0100

    remove useless import

--- src/app/materials/materials.module.ts
+++ src/app/materials/materials.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatDialogModule } from '@angular/material/dialog';
--- src/app/weather/weather.service.ts
+++ src/app/weather/weather.service.ts
@@ -1,7 +1,6 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Router } from '@angular/router';
-import { catchError, Observable, takeUntil, throwError } from 'rxjs';
+import { catchError, Observable, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
